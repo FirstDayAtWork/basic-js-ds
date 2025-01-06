e704d9a327776f9526a6b7a7817c38aaf2bdfb97
Michael <fatlycka@gmail.com>
2025-01-06 04:27:43 +0300

    feat: add "add" method to BST

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,34 +1,56 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  constructor() {
+    this.head = null;
+    this.targetNode = this.head;
+  }
   root() {
+    return this.head;
+  }
+
+  add(data) {
+    if (!this.head) {
+      this.head = new Node(data);
+      this.targetNode = this.head;
+      return this;
+    }
+    if ((data < this.targetNode.data) && this.targetNode.left) {
+      this.targetNode = this.targetNode.left;
+      this.add(data);
+    } else if (data < this.targetNode.data) {
+      this.targetNode.left = new Node(data);
+      this.targetNode = this.head;
+      return this;
+    }
+    if ((data > this.targetNode.data) && this.targetNode.right) {
+      this.targetNode = this.targetNode.right;
+      this.add(data);
+    } else if (data > this.targetNode.data) {
+      this.targetNode.right = new Node(data);
+      this.targetNode = this.head;
+      return this;
+    }
+    return this;
+  }
+
+  has(data) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  add(/* data */) {
+  find(data) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
+  remove(data) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
